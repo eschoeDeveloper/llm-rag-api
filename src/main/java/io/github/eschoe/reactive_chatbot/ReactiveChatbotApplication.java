0b450591ecae3280bfb4653f968d25404670e2e3
ChoeEuiSeung <88296182+eschoeDeveloper@users.noreply.github.com>
2025-08-18 11:06:53 +0900

    feat(proejct) : 최초 push

---
+++ src/main/java/io/github/eschoe/reactive_chatbot/ReactiveChatbotApplication.java
@@ -0,0 +1,13 @@
+package io.github.eschoe.reactive_chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveChatbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveChatbotApplication.class, args);
+    }
+
+}
